db4f82ced0f27e3888bd3db004061dc924d800be
tim <sun.kensin@gmail.com>
2018-05-25 16:58:42 +1200

    add express structure

---
+++ index.js
@@ -0,0 +1,14 @@
+import http from 'http';
+import express from 'express';
+// Express app setup
+const app = express();
+const server = http.createServer(app);
+server.listen(3000);
+server.on('listening', () => {
+  console.log('Server is listening on port: 3000');
+});
+
+// request handler
+app.get('*', (req, res) => {
+    res.end('Hello Express');
+});
